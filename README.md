affd4cf8fa62217565d81cbb78582de9069e8f43
Suraj <subaka1415@gmail.com>
2025-10-16 14:30:53 +0530

    Innitial commit with README and main.py

---
+++ README.md
@@ -0,0 +1,1 @@
+<b> Here I am writing the python code for my assignment.....</b>
---
+++ main.py
@@ -0,0 +1,7 @@
+print("Welcome to my github")
+
+a = int(input("Enter the age : "))
+if a<18:
+    print("You are under 18")
+else:
+    print("You are Adult")
